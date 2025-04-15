7a5794b9b0d1ca8882774421776c7c7798de4994
Cristiano Costa <107226337+CCostaDev@users.noreply.github.com>
2025-04-15 12:11:54 +0100

    file.java: Now 100% more Java-y

---
+++ file.java
@@ -0,0 +1,7 @@
+public class File {
+    public static void main(String[] args) {
+        System.out.println("Step 1: Open Java file.");
+        System.out.println("Step 2: ???");
+        System.out.println("Step 3: Profit.");
+    }
+}
